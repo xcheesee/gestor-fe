6b82c24bc468e371d34388469d54844705fbc1d7
Deivison Leite de Macedo <deivisonmacedo@prefeitura.sp.gov.br>
2022-07-21 13:56:18 -0300

    Adição do botão de adicionar mês ao campo de execução financeira

--- src/components/DadosContrato/ExecucaoFinanceira/ExecucaoFinanceira.jsx
+++ src/components/DadosContrato/ExecucaoFinanceira/ExecucaoFinanceira.jsx
@@ -6,9 +6,11 @@ import {
     Paper,
     IconButton,
     Tooltip,
-    CircularProgress
+    CircularProgress,
+    Button
 } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import DialogDetalhes from './DialogDetalhes/DialogDetalhes';
 
@@ -48,115 +50,162 @@ const ExecucaoFinanceira = (props) => {
     const Conteudo = () => {
         if (Object.keys(execucao_financeira).length > 0) {
             return (
-                <Box 
-                    sx={{ 
-                        padding: '1rem', 
-                        margin: '2rem 0', 
-                        display: 'flex', 
-                        background: '#F8FAF8', 
-                        overflow: 'auto' 
-                    }} 
+                <Box
                     component={Paper} 
                     elevation={5}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        background: '#F8FAF8',
+                        margin: '2rem 0', 
+                    }}
                 >
-                    {Object.keys(execucao_financeira).map((execucao, index) => {
-                        return (
-                            <Box
-                                elevation={3}
-                                component={Paper}
-                                sx={{ 
-                                    padding: '1rem', 
-                                    margin: '0 1rem', 
-                                    minWidth: '16rem'
-                                }}
-                                key={index}
-                            >
-                                <Divider
-                                    textAlign='right'
-                                    sx={{
-                                        fontWeight: 'light',
-                                        fontSize: '1rem',
-                                        mb: '1rem'
+                    <Box
+                        sx={{ 
+                            padding: '1rem',
+                            display: 'flex',
+                            overflow: 'auto' 
+                        }} 
+                    >
+                        {Object.keys(execucao_financeira).map((execucao, index) => {
+                            return (
+                                <Box
+                                    elevation={3}
+                                    component={Paper}
+                                    sx={{ 
+                                        padding: '1rem',
+                                        minWidth: '16rem',
+                                        mr: '1rem'
                                     }}
+                                    key={index}
                                 >
-                                    {execucao_financeira[execucao].mes}
-                                </Divider>
-                                
-                                <Box sx={{ display: 'flex' }}>
-                                    <Box>
-                                        <Typography sx={{ fontWeight: 'medium' }} component="span">
-                                            Planejado
-                                            <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
-                                                {props.formataValores(execucao_financeira[execucao].planejado)}
-                                            </Typography>
-                                        </Typography>
-                    
-                                        <Typography sx={{ fontWeight: 'medium' }} component="span">
-                                            Contratado
-                                            <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
-                                                {props.formataValores(execucao_financeira[execucao].contratado)}
-                                            </Typography>
-                                        </Typography>
-                    
-                                        <Typography sx={{ fontWeight: 'medium' }} component="span">
-                                            Empenhado
-                                            <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
-                                                {props.formataValores(execucao_financeira[execucao].empenhado)}
-                                            </Typography>
-                                        </Typography>
-                    
-                                        <Typography sx={{ fontWeight: 'medium' }} component="span">
-                                            Executado
-                                            <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
-                                                {props.formataValores(execucao_financeira[execucao].executado)}
-                                            </Typography>
-                                        </Typography>
-                    
-                                        <Typography sx={{ fontWeight: 'medium' }} component="span">
-                                            Saldo
-                                            <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
-                                                {props.formataValores(execucao_financeira[execucao].saldo)}
-                                            </Typography>
-                                        </Typography>
-                                    </Box>
-                                    
-                                    <Box 
-                                        sx={{ 
-                                            alignSelf: 'flex-end', 
-                                            width: '100%', 
-                                            display: 'flex', 
-                                            justifyContent: 'flex-end' 
+                                    <Divider
+                                        textAlign='right'
+                                        sx={{
+                                            fontWeight: 'light',
+                                            fontSize: '1rem',
+                                            mb: '1rem'
                                         }}
                                     >
-                                        <Tooltip title="Detalhes" arrow>
-                                            <IconButton
-                                                onClick={() => detalhaExecucaoFinanceira(execucao_financeira[execucao].id)}
-                                            >
-                                                {
-                                                    carregando
-                                                    ? <CircularProgress size="1.5rem" />
-                                                    : <ManageSearchIcon />
-                                                }
-                                            </IconButton>
-                                        </Tooltip>
+                                        {execucao_financeira[execucao].mes}
+                                    </Divider>
+                                    
+                                    <Box sx={{ display: 'flex' }}>
+                                        <Box>
+                                            <Typography sx={{ fontWeight: 'medium' }} component="span">
+                                                Planejado
+                                                <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
+                                                    {props.formataValores(execucao_financeira[execucao].planejado)}
+                                                </Typography>
+                                            </Typography>
+                        
+                                            <Typography sx={{ fontWeight: 'medium' }} component="span">
+                                                Contratado
+                                                <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
+                                                    {props.formataValores(execucao_financeira[execucao].contratado)}
+                                                </Typography>
+                                            </Typography>
+                        
+                                            <Typography sx={{ fontWeight: 'medium' }} component="span">
+                                                Empenhado
+                                                <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
+                                                    {props.formataValores(execucao_financeira[execucao].empenhado)}
+                                                </Typography>
+                                            </Typography>
+                        
+                                            <Typography sx={{ fontWeight: 'medium' }} component="span">
+                                                Executado
+                                                <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
+                                                    {props.formataValores(execucao_financeira[execucao].executado)}
+                                                </Typography>
+                                            </Typography>
+                        
+                                            <Typography sx={{ fontWeight: 'medium' }} component="span">
+                                                Saldo
+                                                <Typography sx={{ padding: '0 1rem', mb: '0.5rem' }}>
+                                                    {props.formataValores(execucao_financeira[execucao].saldo)}
+                                                </Typography>
+                                            </Typography>
+                                        </Box>
                                         
-                                        <Tooltip title="Editar" arrow>
-                                            <IconButton>
-                                                <EditIcon fontSize="small" />
-                                            </IconButton>
-                                        </Tooltip>
+                                        <Box 
+                                            sx={{ 
+                                                alignSelf: 'flex-end', 
+                                                width: '100%', 
+                                                display: 'flex', 
+                                                justifyContent: 'flex-end' 
+                                            }}
+                                        >
+                                            <Tooltip title="Detalhes" arrow>
+                                                <IconButton
+                                                    onClick={() => detalhaExecucaoFinanceira(execucao_financeira[execucao].id)}
+                                                    disabled={carregando}
+                                                >
+                                                    {
+                                                        carregando
+                                                        ? <CircularProgress size="1.5rem" />
+                                                        : <ManageSearchIcon />
+                                                    }
+                                                </IconButton>
+                                            </Tooltip>
+                                            
+                                            <Tooltip title="Editar" arrow>
+                                                <IconButton>
+                                                    <EditIcon fontSize="small" />
+                                                </IconButton>
+                                            </Tooltip>
+                                        </Box>
                                     </Box>
                                 </Box>
-                            </Box>
-                        );
-                    })}
+                            );
+                        })}
+                    </Box>
+                    
+                    <Box
+                        sx={{
+                            alignSelf: 'flex-end',
+                            padding: '1rem',
+                            pt: 0,
+                        }}
+                    >
+                        <Button
+                            sx={{
+                                textTransform: 'none'
+                            }}
+                        >
+                            <AddIcon sx={{ mr: '0.2rem' }} />
+                            Adicionar mês
+                        </Button>
+                    </Box>
                 </Box>
             );
         } else {
             return (
-                <Box sx={{ padding: '1rem', margin: '2rem 0', display: 'flex', background: '#FFFFFF', overflow: 'auto' }} component={Paper} elevation={5}>
-                    - - -
+                <Box
+                    component={Paper} 
+                    elevation={5}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        background: '#F8FAF8',
+                        margin: '2rem 0', 
+                    }}
+                >
+                    <Typography sx={{ margin: '1rem' }}>
+                        Nenhum dado de execução financeira disponível para este contrato
+                    </Typography>
+
+                    <Button
+                        sx={{
+                            alignSelf: 'flex-end',
+                            textTransform: 'none',
+                        }}
+                    >
+                        <AddIcon sx={{ mr: '0.2rem' }} />
+                        Adicionar mês
+                    </Button>
                 </Box>
+
             );
         }
     }
