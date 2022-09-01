0c2ba5e4296850ef45592d4ff30d1adf7bc8e67d
João Lucas Silva Laurindo Cruz <x529427@rede.sp>
2022-09-01 11:13:24 -0300

    cleanup e comments

--- src/helpers/CalendarioRange.js
+++ src/helpers/CalendarioRange.js
@@ -5,8 +5,9 @@ import './calendarioRange.css'
 import pt_BR from 'rsuite/locales/pt_BR'
 
 const CalendarioRange = ({intervalo, onChange, label, separador, size, rangeStart, rangeEnd, placeholder}) => {
+    //input recebido e convertido ao formato permitido de DateRangePicker. 
+    //'T00:00:00' previne que datas sejam alteradas devido ao conflito de timezones e a implementacao de new Date()
     const datas = intervalo[0] === '' ? [null , null] : [new Date(intervalo[0]+'T00:00:00'), new Date(intervalo[1]+'T00:00:00')];
-    console.log(datas)
     
     return (
         <div className='inputContainer'>
@@ -18,8 +19,10 @@ const CalendarioRange = ({intervalo, onChange, label, separador, size, rangeStar
                     format={"dd/MM/yyyy"}
                     placeholder={placeholder}
                     onChange={e => {
+                        /* funcao clean retorna null, que entao é transformado em array para integracao com onChange */
                         e === null ? onChange(rangeStart, rangeEnd, ['', ''])
-                        : onChange(rangeStart, rangeEnd, [e[0]?.toLocaleDateString('fr-CA'), e[1]?.toLocaleDateString('fr-CA')])
+                        /* conversao de datas para aaaa-mm-dd */
+                        : onChange(rangeStart, rangeEnd, [e[0]?.toLocaleDateString('fr-CA') , e[1]?.toLocaleDateString('fr-CA')])
                         
                     }}
                     size={size}
