2e22a02dca9de9f1e55a256a937a0a784bf74736
João Lucas Silva Laurindo Cruz <x529427@rede.sp>
2022-09-02 09:25:00 -0300

    cleanup

--- src/components/CampoDataRange/CampoDataRange.jsx
+++ src/components/CampoDataRange/CampoDataRange.jsx
@@ -4,10 +4,12 @@ import 'rsuite/dist/rsuite.min.css';
 import './calendarioRange.css'
 import pt_BR from 'rsuite/locales/pt_BR'
 
-const CampoDataRange = ({intervalo,filtro,  onChange, label, separador, size,/*  rangeStart, rangeEnd, */ placeholder}) => {
+const CampoDataRange = ({intervalo, filtro, onChange, label, separador, size, placeholder}) => {
     //input recebido e convertido ao formato permitido de DateRangePicker. 
     //'T00:00:00' previne que datas sejam alteradas devido ao conflito de timezones e a implementacao de new Date()
-    const datas = filtro[intervalo.inicio] === '' ? [null , null] : [new Date(filtro[intervalo.inicio]+'T00:00:00'), new Date(filtro[intervalo.fim]+'T00:00:00')];
+    const datas = filtro[intervalo.inicio] === '' 
+        ? [null , null] 
+        : [new Date(filtro[intervalo.inicio]+'T00:00:00'), new Date(filtro[intervalo.fim]+'T00:00:00')];
     
     return (
         <div className='inputContainer'>
@@ -22,6 +24,7 @@ const CampoDataRange = ({intervalo,filtro,  onChange, label, separador, size,/* 
                         e => onChange(intervalo.inicio, intervalo.fim, ['', ''])
                     }
                     onOk={
+                        //tranforma o output em string aaaa-mm-dd e envia-o para setFiltros
                         e => onChange(intervalo.inicio, intervalo.fim, [e[0]?.toLocaleDateString('fr-CA') , e[1]?.toLocaleDateString('fr-CA')])
                     }
                     size={size}
